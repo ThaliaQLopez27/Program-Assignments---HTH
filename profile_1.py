fbc00d653f5bebf9e869478b038a457114d22366
Thalia Lopez <thalialopez@Thalias-MacBook-Air.local>
2026-06-18 16:22:39 -0700

    Initial VR project upload

---
+++ profile_1.py
@@ -0,0 +1,9 @@
+full_name = "Thalia Lopez"
+nickname = "Toodles"
+age = 17
+used_python_before = False
+hobbies = ["volleyball", "playing roblox", "going to JROTC events"]
+favorites = {"movie" :"Forest Gump", "food" : "cucumbers", "hobby" : "going to JROTC events"}
+best_friends = ["Abby", "Huffman", "Bymaster", "Dasani", "Mila"]
+
+print(full_name, nickname, age, used_python_before, hobbies, favorites,best_friends)
